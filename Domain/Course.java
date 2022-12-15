c2b0cbb4029d05d53f82ff6b0ca453547bdab4ab
samadov2201 <muhammadsamadov@gmail.com>
2022-12-15 11:38:47 +0100

    hoi

---
+++ Domain/Course.java
@@ -0,0 +1,13 @@
+package Domain;
+
+import java.util.ArrayList;
+
+public class Course {
+    private ArrayList contentList;
+    private String name;
+    private String subject;
+    private String introText;
+    private String difficulty;
+    private ArrayList recommendedCourses;
+
+}
