ce6991b5881740f516f9dd7bd4296e4ac48813a4
Steen010 <kjh.vansteen@student.avans.nl>
2023-03-19 17:46:25 +0100

    Buttons aangemaakt voor de overzichten, hoofdletters aangepast.

--- Gui/ApplicationController.java
+++ Gui/ApplicationController.java
@@ -17,6 +17,7 @@ public class ApplicationController extends Application {
     Scene registrations = new RegistrationController().printRegistration();
     Scene modules = new ModuleController().printModules();
     Scene webcasts = new WebcastController().printModules();
+    Scene overviews = new OverviewController().printOverviews();
 
     Stage window;
 
@@ -26,33 +27,37 @@ public class ApplicationController extends Application {
         BorderPane layout = new BorderPane();
         VBox list = new VBox();
         list.getChildren().add(new Label("Welkom!"));
-        list.getChildren().add(new Label("kies een van de volgende acties:"));
+        list.getChildren().add(new Label("Kies een van de volgende acties:"));
         HBox buttons = new HBox();
-        Button Courses = new Button("cursussen zien en wijzigen");
+        Button Courses = new Button("Cursussen zien en wijzigen");
         Courses.setOnAction((Action) -> {
             window.setScene(courses);
         });
-        Button cursists = new Button("cursisten zien en wijzigen");
+        Button cursists = new Button("Cursisten zien en wijzigen");
         cursists.setOnAction((Action) -> {
             window.setScene(this.cursists);
         });
-        Button registrations = new Button("inschrijvingen zien en wijzigen");
+        Button registrations = new Button("Inschrijvingen zien en wijzigen");
         registrations.setOnAction((Action) -> {
             window.setScene(this.registrations);
         });
-        Button certificates = new Button("diploma's zien en wijzigen");
+        Button certificates = new Button("Diploma's zien en wijzigen");
         certificates.setOnAction((Action) -> {
             window.setScene(this.certificates);
         });
-        Button modules = new Button("modules zien en wijzigen");
+        Button modules = new Button("Modules zien en wijzigen");
         modules.setOnAction((Action) -> {
             window.setScene(this.modules);
         });
-        Button webcasts = new Button("webcasts zien en wijzigen");
+        Button webcasts = new Button("Webcasts zien en wijzigen");
         webcasts.setOnAction((Action) -> {
             window.setScene(this.webcasts);
         });
-        buttons.getChildren().addAll(Courses, cursists, registrations, certificates, modules, webcasts);
+        Button overview = new Button("Laat overzichten zien");
+        overview.setOnAction((Action) -> {
+            window.setScene(this.overviews);
+        });
+        buttons.getChildren().addAll(Courses, cursists, registrations, certificates, modules, webcasts, overview);
         list.getChildren().add(buttons);
         layout.setCenter(list);
         this.index = new Scene(layout);
---
+++ Gui/OverviewController.java
@@ -0,0 +1,86 @@
+package Gui;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class OverviewController {
+
+    Scene index;
+
+    public Scene printOverviews() {
+        BorderPane layout = new BorderPane();
+        Scene printOverviews = new Scene(layout);
+        VBox list = new VBox();
+        list.getChildren().add(new Label("Welkom!"));
+        list.getChildren().add(new Label("kies een van de volgende acties:"));
+        HBox buttons = new HBox();
+        HBox button2 = new HBox();
+        Button back = new Button("Ga terug");
+
+        back.setOnAction((Action) -> {
+            Stage stage = new Stage();
+            ApplicationController controller = new ApplicationController();
+            try {
+                controller.start(stage);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            Node node = (Node) Action.getSource();
+            Stage thisStage = (Stage) node.getScene().getWindow();
+            thisStage.close();
+        });
+
+        Button geslacht = new Button("% Behaalde cursussen per geslacht");
+        geslacht.setOnAction((Action) -> {
+            // functionaliteit
+        });
+        Button gemiddeld = new Button("Gemiddelde voortgang per cursus en per module");
+        gemiddeld.setOnAction((Action) -> {
+            // functionaliteit
+        });
+        Button accountCursus = new Button("Voortgang cursus en modules per account");
+        accountCursus.setOnAction((Action) -> {
+            // functionaliteit
+        });
+        Button account = new Button("Behaalde certificaten per account");
+        account.setOnAction((Action) -> {
+            // functionaliteit
+        });
+        Button webcasts = new Button("Top 3 meest bekeken webcasts");
+        webcasts.setOnAction((Action) -> {
+            // functionaliteit
+        });
+        Button behaaldeCursussen = new Button("Top 3 meest behaalde cursussen");
+        behaaldeCursussen.setOnAction((Action) -> {
+            // functionaliteit
+        });
+        Button aanbevolen = new Button("Aanbevolen cursussen");
+        aanbevolen.setOnAction((Action) -> {
+            // functionaliteit
+        });
+        Button geslaagde = new Button("Aantal geslaagde cursisten per cursus");
+        geslaagde.setOnAction((Action) -> {
+            // functionaliteit
+        });
+        buttons.getChildren().addAll(geslacht, gemiddeld, accountCursus, account);
+        button2.getChildren().addAll(back, webcasts, behaaldeCursussen, aanbevolen, geslaagde);
+        list.getChildren().addAll(buttons, button2);
+        layout.setCenter(list);
+        return printOverviews;
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setScene(printOverviews());
+        stage.show();
+
+    }
+}
